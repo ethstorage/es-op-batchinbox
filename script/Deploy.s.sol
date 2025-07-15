d10e4959c2a139d7c36deccab03f1e23822259f3
qiang <freedostudio@gmail.com>
2025-07-15 16:36:15 +0800

    use upgradable plugin

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -1,13 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/proxy/transparent/TransparentUpgradeableProxy.sol";
-import "@openzeppelin/proxy/transparent/ProxyAdmin.sol";
+import {Script} from "forge-std/Script.sol";
+import {console} from "forge-std/console.sol";
 
-import "forge-std/Script.sol";
-import "forge-std/console.sol";
-
-import "../src/BatchInbox.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
+import {BatchInbox} from "../src/BatchInbox.sol";
 
 contract Deploy is Script {
     function setUp() public {}
@@ -17,38 +15,27 @@ contract Deploy is Script {
         address ownerAddress = vm.envAddress("OWNER_ADDRESS");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy the implementation contract
-        BatchInbox implementation = new BatchInbox();
-
-        // Encode the initialization data for the implementation contract
-        bytes memory data = abi.encodeWithSelector(BatchInbox.initialize.selector, ownerAddress);
-
-        // Deploy the proxy pointing to the implementation
-        TransparentUpgradeableProxy proxy = new TransparentUpgradeableProxy(address(implementation), ownerAddress, data);
+        address proxy = Upgrades.deployTransparentProxy(
+            "BatchInbox.sol:BatchInbox",
+            ownerAddress,
+            abi.encodeWithSelector(BatchInbox.initialize.selector, ownerAddress)
+        );
 
         vm.stopBroadcast();
 
         // Log the addresses
-        console.log("BatchInbox Implementation Address:", address(implementation));
         console.log("BatchInbox Proxy Address:", address(proxy));
         console.log("BatchInbox Owner Address:", ownerAddress);
     }
 
-    function upgrade(address proxyAddress, address proxyAdminAddress) public {
+    function upgrade(address proxyAddress) public {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        // Deploy a new implementation
-        BatchInbox newImplementation = new BatchInbox();
-
-        // Get the ProxyAdmin instance
-        ProxyAdmin admin = ProxyAdmin(proxyAdminAddress);
-
-        // Upgrade the proxy to use the new implementation
-        admin.upgradeAndCall(ITransparentUpgradeableProxy(proxyAddress), address(newImplementation), "0x");
+        Upgrades.upgradeProxy(proxyAddress, "BatchInbox.sol:BatchInbox", "");
 
         vm.stopBroadcast();
 
-        console.log("Upgraded proxy at %s to new implementation at %s", proxyAddress, address(newImplementation));
+        console.log("Upgraded proxy at %s to new implementation", proxyAddress);
     }
 }
