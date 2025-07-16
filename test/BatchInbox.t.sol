d500812aababbe02ddbabb3b99404a5745f1de19
qiang <freedostudio@gmail.com>
2025-07-16 15:37:49 +0800

    initialize and owner test

--- test/BatchInbox.t.sol
+++ test/BatchInbox.t.sol
@@ -3,12 +3,17 @@ pragma solidity ^0.8.24;
 
 import {Test} from "forge-std/Test.sol";
 import {BatchInbox} from "../src/BatchInbox.sol";
+import {Upgrades} from "openzeppelin-foundry-upgrades/Upgrades.sol";
 
 contract BatchInboxTest is Test {
     BatchInbox public batchInbox;
 
     function setUp() public {
-        batchInbox = new BatchInbox();
+        address owner = address(this);
+        address proxy = Upgrades.deployTransparentProxy(
+            "BatchInbox.sol:BatchInbox", owner, abi.encodeCall(BatchInbox.initialize, (owner))
+        );
+        batchInbox = BatchInbox(payable(proxy));
     }
 
     function testDepositWithdrawOK() public {
@@ -46,5 +51,26 @@ contract BatchInboxTest is Test {
         vm.prank(target);
         vm.expectRevert(BatchInbox.BalanceNotEnough.selector);
         batchInbox.withdraw(target, 2 ether);
+    }
+
+    function testInitializeSetsOwner() public view {
+        assertEq(batchInbox.owner(), address(this));
+    }
+
+    function testInitializeCanOnlyBeCalledOnce() public {
+        vm.expectRevert(abi.encodeWithSignature("InvalidInitialization()"));
+        batchInbox.initialize(address(0x123));
+    }
+
+    function testOnlyOwnerCanSetStorageContract() public {
+        address nonOwner = address(0x456);
+        vm.prank(nonOwner);
+        vm.expectRevert(abi.encodeWithSignature("OwnableUnauthorizedAccount(address)", nonOwner));
+        batchInbox.setEsStorageContract(address(0x789));
+
+        // Owner can set the storage contract
+        address storageContract = address(0x789);
+        batchInbox.setEsStorageContract(storageContract);
+        assertEq(address(batchInbox.esStorageContract()), storageContract);
     }
 }
